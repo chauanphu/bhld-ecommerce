8fac2d7fa9d794ee249191e0e94c5739f255e7e3
chauanphu <chauanphu@gmail.com>
2022-07-20 14:32:15 +0700

    connect db

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -4,7 +4,27 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   console.log(req.body);
-  res.send("OK");
+  res.send("Got All");
+});
+
+router.get("/:id", (req, res) => {
+  console.log(req.params);
+  res.send(`Got ${req.id}`);
+});
+
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Posted");
+});
+
+router.patch("/:id", (req, res) => {
+  console.log(req.params);
+  res.send(`Patched ${req.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+  console.log(req.body);
+  res.send("Deleted");
 });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+const mongoose = require("mongoose");
 const workout = require("./routes/workout");
 
 const app = express();
@@ -9,6 +10,16 @@ app.use(express.json());
 
 //routes
 app.use("/api/workout", workout);
+
+//connecting to db
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error: " + err);
+  });
 
 //set up
 app.listen(process.env.PORT, () => {
