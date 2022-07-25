c730c2954805b8671170f9fd70bd7a3a9ee38786
chauanphu <chauanphu@gmail.com>
2022-07-25 10:44:49 +0700

    add product.jsx

---
+++ frontend/src/components/Products.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+
+const Products = () => {
+  return (
+    <main>
+      hello
+      <Grid container justify="center" spacing={4}></Grid>
+    </main>
+  );
+};
+
+export default Products;
