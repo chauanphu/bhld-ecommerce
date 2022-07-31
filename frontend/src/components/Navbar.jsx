67c58de0006858addf4289d7800bc87722ce9537
chauanphu <chauanphu@gmail.com>
2022-07-31 20:39:05 +0700

    finish responsive navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { useState } from 'react';
-import { AppBar, Toolbar, Tabs, Tab, Stack, Typography } from "@mui/material";
+import { AppBar, Toolbar, Tabs, Tab, Stack, Typography, Skeleton, IconButton, Drawer } from "@mui/material";
 import { Link } from "react-router-dom";
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Navbar = () => {
     const [tabvalue, setTab] = useState('0');
+    const [drawer, setDrawer] = useState(false)
     const handleTabChange = (event, newValue) => {
+        setDrawer(false)
         setTab(newValue)
     };
+    const toggleDrawer = (event, newValue) => {
+        setDrawer(newValue)
+    }
     const tabs = [
         { label: 'Trang chu', path: '/', value: '0' },
         { label: 'San pham', path: '/products', value: '1' },
@@ -18,7 +24,7 @@ const Navbar = () => {
             <AppBar sx={{
                 height: "30px",
                 backgroundColor: 'gray',
-                position: 'static'
+                position: 'static',
             }}>
                 <Stack direction="row" spacing={2} sx={{
                     display: "flex",
@@ -33,29 +39,63 @@ const Navbar = () => {
             <AppBar sx={{
                 height: "80px",
                 backgroundColor: 'white',
-                position: 'static',
+                position: 'relative',
+                display: 'flex',
+                justifyContent: 'center'
             }}>
-                <Toolbar>
-                    <Tabs value={tabvalue} onChange={handleTabChange} centered={true}
-                        sx={{
-                            width: '100%',
-                        }}
-                    >
+                {/* Logo */}
+                <IconButton size="large" onClick={event => toggleDrawer(event, true)} sx={{
+                    display: { md: 'none' },
+                    position: 'absolute',
+                }}>
+                    <MenuIcon />
+                </IconButton>
+                <Skeleton sx={{
+                    width: 100,
+                    height: 70,
+                    position: { xs: 'absolute', md: 'block' },
+                    margin: { xs: 'auto', md: '0 0 0 50px' },
+                    ml: 5,
+                    left: 0,
+                    right: 0,
+                }}></Skeleton>
+                <Toolbar sx={{
+                    width: 'fit-content',
+                    visibility: {
+                        xs: 'hidden',
+                        md: 'visible',
+                    },
+                    position: 'absolute',
+                    margin: 'auto',
+                    left: 0,
+                    right: 0,
+                }}>
+                    <Tabs value={tabvalue} onChange={handleTabChange}>
                         {tabs.map((tab) => (
-                            // <Link key={tab.value} style={{ textDecoration: 'none' }} to={tab.path}>
                             <Tab key={tab.value} component={Link} to={tab.path} label={tab.label} value={tab.value} sx={{
                                 typography: {
                                     md: 'h6'
                                 },
                                 color: 'green'
                             }} />
-                            // </Link>
                         )
                         )}
-
                     </Tabs>
                 </Toolbar>
             </AppBar>
+            <Drawer anchor='left' open={drawer} onClose={event => toggleDrawer(event, false)}>
+                <Tabs value={tabvalue} orientation="vertical" onChange={handleTabChange}>
+                    {tabs.map((tab) => (
+                        <Tab key={tab.value} component={Link} to={tab.path} label={tab.label} value={tab.value} sx={{
+                            typography: {
+                                md: 'h6'
+                            },
+                            color: 'green'
+                        }} />
+                    )
+                    )}
+                </Tabs>
+            </Drawer>
         </>
     )
 }
