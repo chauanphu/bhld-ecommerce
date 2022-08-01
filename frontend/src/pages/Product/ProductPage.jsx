11f2dd64cd2df84326479d75729cc769c0cae5db
chauanphu <chauanphu@gmail.com>
2022-08-01 09:30:47 +0700

    responsive footer

--- frontend/src/pages/Product/ProductPage.jsx
+++ frontend/src/pages/Product/ProductPage.jsx
@@ -1,11 +1,29 @@
+import { Breadcrumbs, Link, Typography, Container, Box } from "@mui/material"
 import { Routes, Route } from "react-router-dom"
 const ProductPage = () => {
     return (
         <>
-            Product
-            <Routes>
-                <Route path="all" element={<>Hello</>}></Route>
-            </Routes>
+            <Container maxWidth='xl'>
+                <Breadcrumbs aria-label="breadcrumb" sx={{ my: 3 }} separator=">">
+                    <Link underline="hover" color="inherit" href="/">
+                        MUI
+                    </Link>
+                    <Link
+                        underline="hover"
+                        color="inherit"
+                        href="/material-ui/getting-started/installation/"
+                    >
+                        Core
+                    </Link>
+                    <Typography color="text.primary">Breadcrumbs</Typography>
+                </Breadcrumbs>
+                <Box display="flex" flexDirection="column">
+
+                    <Routes>
+                        <Route path="/" element={<>Hello</>}></Route>
+                    </Routes>
+                </Box>
+            </Container>
         </>
     )
 }
