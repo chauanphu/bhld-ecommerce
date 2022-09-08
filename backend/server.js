a6d8e63f65ec125a7d0d38c86ce2de9e7f0a1041
chauanphu <chauanphu@gmail.com>
2022-09-08 13:45:54 +0700

    dockerize it

--- backend/server.js
+++ backend/server.js
@@ -13,16 +13,6 @@ app.use(cors())
 //routes
 app.use("/api", routes);
 
-//connecting to db
-// mongoose
-//   .connect(process.env.MONGODB_URL)
-//   .then(() => {
-//     console.log("Connected to database");
-//   })
-//   .catch((err) => {
-//     console.log("Error: " + err);
-//   });
-
 //set up
 app.listen(process.env.PORT, () => {
   console.log("Listening on port " + process.env.PORT);
