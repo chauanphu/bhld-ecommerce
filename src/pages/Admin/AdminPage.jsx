733d085406003b93ba03f8a9228af31e692f1c91
chauanphu <chauanphu@gmail.com>
2022-10-03 07:33:15 +0700

    build a custome dataProvider

--- src/pages/Admin/AdminPage.jsx
+++ src/pages/Admin/AdminPage.jsx
@@ -1,15 +1,108 @@
 import { Admin, Resource } from 'react-admin'
-import restProvider from 'ra-data-simple-rest'
-
 import { ProductList, ProductCreate, ProductEdit } from './'
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 import LoginPage from "./LoginPage";
+import axios from 'axios'
 
-const url_link = 'http://localhost:8000/api'
+const apiUrl = 'http://localhost:8000/api'
+
+const dataProvider = {
+    getList: (resource, params) => {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+            sort: JSON.stringify([field, order]),
+            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+            filter: JSON.stringify(params.filter),
+        };
+        const url = `${apiUrl}/${resource}?${JSON.stringify(query)}`;
+
+        return axios.get(url).then(({ headers, data }) => ({
+            data: data.map(resource => ({ ...resource, id: resource._id })),
+            total: parseInt(headers['content-range'].split('/').pop(), 10),
+        }));
+    },
+    getOne: (resource, params) =>
+        axios.get(`${apiUrl}/${resource}/${params.id}`).then(({ data }) => ({
+            data: { ...data, id: data._id },
+        })),
+
+    getMany: (resource, params) => {
+        const query = {
+            filter: JSON.stringify({ id: params.ids }),
+        };
+        const url = `${apiUrl}/${resource}?${JSON.stringify(query)}`;
+        return axios(url).then(({ data }) => ({
+            data: data.map(resource => ({ ...resource, id: resource._id })),
+        }));
+    },
+
+    getManyReference: (resource, params) => {
+        const { page, perPage } = params.pagination;
+        const { field, order } = params.sort;
+        const query = {
+            sort: JSON.stringify([field, order]),
+            range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
+            filter: JSON.stringify({
+                ...params.filter,
+                [params.target]: params.id,
+            }),
+        };
+        const url = `${apiUrl}/${resource}?${JSON.stringify(query)}`;
+
+        return axios(url).then(({ headers, data }) => ({
+            data: data.map(resource => ({ ...resource, id: resource._id })),
+            total: parseInt(headers.get('content-range').split('/').pop(), 10),
+        }));
+    },
+
+    update: (resource, params) =>
+        axios(`${apiUrl}/${resource}/${params.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(params.data),
+        }).then(({ data }) => ({
+            data: { ...data, id: data._id },
+        })),
+
+    updateMany: (resource, params) => {
+        const query = {
+            filter: JSON.stringify({ id: params.ids }),
+        };
+        return axios(`${apiUrl}/${resource}?${JSON.stringify(query)}`, {
+            method: 'PUT',
+            body: JSON.stringify(params.data),
+        }).then(({ data }) => ({ data: data }));
+    },
+
+    create: (resource, params) =>
+        axios(`${apiUrl}/${resource}`, {
+            method: 'POST',
+            body: JSON.stringify(params.data),
+        }).then(({ data }) => ({
+            data: { ...params.data, id: data._id },
+        })),
+
+    delete: (resource, params) =>
+        axios(`${apiUrl}/${resource}/${params.id}`, {
+            method: 'DELETE',
+        }).then(({ data }) => ({
+            data: { ...data, id: data._id },
+        })),
+
+    deleteMany: (resource, params) => {
+        const query = {
+            filter: JSON.stringify({ id: params.ids }),
+        };
+        return axios(`${apiUrl}/${resource}?${JSON.stringify(query)}`, {
+            method: 'DELETE',
+            body: JSON.stringify(params.data),
+        }).then(({ data }) => ({ data: data }));
+    }
+};
 
 const AdminPage = () => {
     return (
-        <Admin loginPage={LoginPage} basename="/admin" dataProvider={restProvider(url_link)}>
+        <Admin loginPage={LoginPage} basename="/admin" dataProvider={dataProvider}>
             <Resource icon={ShoppingBasketIcon} name="products" list={ProductList} create={ProductCreate} edit={ProductEdit} />
         </Admin>
     )
